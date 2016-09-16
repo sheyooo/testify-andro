3eac926c119252d0fc3dfa8e961e582f59da109d
Seyi Adekoya <seyi.adekoya@andela.com>
2016-09-16 23:53:53 +0100

    adds decoded jwt model and fblogin payload that can be sent to api

---
+++ app/src/main/java/sheyi/com/testify/models/app/JWTDecoded.java
@@ -0,0 +1,10 @@
+package sheyi.com.testify.models.app;
+
+/**
+ * Created by andela on 16/09/2016.
+ */
+public class JWTDecoded {
+    public String hash_id;
+    public String sub;
+    public String exp;
+}
