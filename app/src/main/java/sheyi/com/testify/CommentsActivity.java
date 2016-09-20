43678715f03e81c47b8581b87319cc9e40ded41a
Seyi Adekoya <seyi.adekoya@andela.com>
2016-09-20 12:01:30 +0100

    modified comments activity

--- app/src/main/java/sheyi/com/testify/CommentsActivity.java
+++ app/src/main/java/sheyi/com/testify/CommentsActivity.java
@@ -2,22 +2,31 @@ package sheyi.com.testify;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+import sheyi.com.testify.adapter.CommentsAdapter;
 import sheyi.com.testify.models.Comment;
 import sheyi.com.testify.rest.ApiClient;
 import sheyi.com.testify.rest.ApiInterface;
 
 public class CommentsActivity extends AppCompatActivity {
+    private RecyclerView commentRecyclerView;
+    private CommentsAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comments);
+
+        commentRecyclerView = (RecyclerView) findViewById(R.id.commentRecyclerView);
+        commentRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
 
         int postID = getIntent().getExtras().getInt("post_id");
 
@@ -27,7 +36,10 @@ public class CommentsActivity extends AppCompatActivity {
         call.enqueue(new Callback<List<Comment>>() {
             @Override
             public void onResponse(Call<List<Comment>> call, Response<List<Comment>> response) {
+                adapter = new CommentsAdapter(CommentsActivity.this, response.body());
+                commentRecyclerView.setAdapter(adapter);
 
+                adapter.setDataSet(response.body());
             }
 
             @Override
@@ -35,5 +47,9 @@ public class CommentsActivity extends AppCompatActivity {
 
             }
         });
+
+
+
+
     }
 }
