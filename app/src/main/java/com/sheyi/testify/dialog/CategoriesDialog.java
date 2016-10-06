ca16e0491e464b8f2009d8b763357226d0864e2b
Seyi Adekoya <seyi.adekoya@andela.com>
2016-10-06 23:26:35 +0100

    improved categories dialog

--- app/src/main/java/com/sheyi/testify/dialog/CategoriesDialog.java
+++ app/src/main/java/com/sheyi/testify/dialog/CategoriesDialog.java
@@ -9,38 +9,36 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ProgressBar;
 
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 import com.sheyi.testify.R;
+import com.sheyi.testify.activity.NewPostActivity;
 import com.sheyi.testify.adapter.CategoryAdapter;
 import com.sheyi.testify.callback.CategoryCallback;
-import com.sheyi.testify.models.Category;
-import com.sheyi.testify.rest.ApiClient;
-import com.sheyi.testify.rest.ApiInterface;
+import com.sheyi.testify.orm.CategoryORM;
+import com.sheyi.testify.util.Application;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CategoriesDialog extends DialogFragment {
     private CategoryCallback callback;
-    private List<Category> data;
+    private List<CategoryORM> data;
     private RecyclerView recyclerView;
     private AlertDialog.Builder builder;
-    private ProgressBar progressBar;
     private Dialog dialog;
     private CategoryAdapter categoryAdapter;
+    private NewPostActivity activity;
 
-    public void setCallback(CategoryCallback callback) {
-        this.callback = callback;
+    private List<CategoryORM> currentlyCheckedCats = new ArrayList<>();
+
+    public void setActivityField(NewPostActivity activity) {
+        this.activity = activity;
     }
-
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        // Use the Builder class for convenient dialog construction
         builder = new AlertDialog.Builder(getActivity());
+
         LayoutInflater inflater = getActivity().getLayoutInflater();
         View view = inflater.inflate(R.layout.dialog_category, null);
 
@@ -48,42 +46,38 @@ public class CategoriesDialog extends DialogFragment {
         return dialog;
     }
 
+    public void setCallback(CategoryCallback callback) {
+        this.callback = callback;
+    }
+
+    public void setCurrentlyChecked(List<CategoryORM> list) {
+        this.currentlyCheckedCats = list;
+    }
+
     private void initializeComponent(View view) {
         builder.setView(view)
                 .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int id) {
-                        callback.onCategorySet(categoryAdapter.getCheckedList());
+                        if (categoryAdapter.getCheckedList() != null) {
+                            callback.onCategorySet(categoryAdapter.getCheckedList());
+                        }
                     }
                 });
 
         dialog = builder.create();
         recyclerView = (RecyclerView) view.findViewById(R.id.categoryList);
-        progressBar = (ProgressBar) view.findViewById(R.id.progress_bar);
-        getData();
-    }
 
-    private void getData() {
-        ApiInterface apiService = ApiClient.getApi(this.getActivity());
-        Call<List<Category>> call = apiService.getCategories();
-        call.enqueue(new Callback<List<Category>>() {
-            @Override
-            public void onResponse(Call<List<Category>> call, Response<List<Category>> response) {
-                data = response.body();
-                progressBar.setVisibility(View.GONE);
-                setUpDialog();
-            }
-
-            @Override
-            public void onFailure(Call<List<Category>> call, Throwable t) {
-
-            }
-        });
+        List<CategoryORM> cats = Application.getCategories();
+        data = cats;
+        setUpDialog();
     }
 
     private void setUpDialog() {
-        categoryAdapter = new CategoryAdapter(getContext(), data);
+        categoryAdapter = new CategoryAdapter(activity, data);
+
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        categoryAdapter.setCheckedList(currentlyCheckedCats);
         recyclerView.setAdapter(categoryAdapter);
     }
 
