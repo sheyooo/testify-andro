403b1974450b92ddcd960ba5e77466c66a226673
Seyi Adekoya <seyi.adekoya@andela.com>
2016-10-06 23:31:12 +0100

    Update util class

--- app/src/main/java/com/sheyi/testify/util/Application.java
+++ app/src/main/java/com/sheyi/testify/util/Application.java
@@ -1,19 +1,51 @@
 package com.sheyi.testify.util;
 
+import android.app.Activity;
+import android.graphics.Typeface;
+import android.view.View;
+import android.widget.TextView;
+
+import com.orm.SugarRecord;
+import com.sheyi.testify.R;
+import com.sheyi.testify.models.Category;
+import com.sheyi.testify.orm.CategoryORM;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import com.sheyi.testify.models.Category;
-
 public class Application {
-    private static List<Category> categories = new ArrayList<>();
 
-    public static List<Category> getCategories() {
-        if (categories.size() < 1) {
-            return categories;
-        } else {
-            return categories;
+    public static TextView generateUICategoryTag(Activity activity, Category tag) {
+        TextView tv = new TextView(activity);
+        tv.setPadding(10, 1, 10, 1);
+        tv.setGravity(View.TEXT_ALIGNMENT_CENTER);
+        tv.setBackgroundResource(R.drawable.tags_bg);
+        tv.setTextColor(activity.getResources().getColor(R.color.white));
+        tv.setText(tag.getName());
+        tv.setTextSize(14);
+        tv.setTypeface(null, Typeface.BOLD);
+
+        return tv;
+    }
+
+    public static void saveCategoriesToDB(List<Category> categories) {
+        List<CategoryORM> cats = new ArrayList<>();
+
+        for(Category cat : categories) {
+            cats.add(new CategoryORM(cat.getId(), cat.getName(), cat.getType(), cat.getSort()));
         }
 
+        SugarRecord.saveInTx(cats);
+    }
+
+    public static List<CategoryORM> getCategories() {
+        List<CategoryORM> cats;
+        try {
+            cats = CategoryORM.listAll(CategoryORM.class);
+        } catch (Exception e) {
+            cats = new ArrayList<>();
+        }
+
+        return cats;
     }
 }
