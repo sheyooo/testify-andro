87714884553e0f753f9fdcaab8986c2849110c40
Seyi Adekoya <seyi.adekoya@andela.com>
2016-10-06 23:27:38 +0100

    added sugar orm initializer

--- app/src/main/java/com/sheyi/testify/TestifyApp.java
+++ app/src/main/java/com/sheyi/testify/TestifyApp.java
@@ -3,10 +3,20 @@ package com.sheyi.testify;
 import android.app.Application;
 
 import com.facebook.stetho.Stetho;
+import com.orm.SugarContext;
 
 public class TestifyApp extends Application {
+
+    @Override
     public void onCreate() {
         super.onCreate();
+        SugarContext.init(getApplicationContext());
         Stetho.initializeWithDefaults(this);
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+        SugarContext.terminate();
     }
 }
