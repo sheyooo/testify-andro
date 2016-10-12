97c12adc3292a4e6a75821c98dd359888917d7ba
Seyi Adekoya <seyi.adekoya@andela.com>
2016-10-12 09:22:24 +0100

    modified navdrawer activity and deletes home fragment

--- app/src/main/java/com/sheyi/testify/activity/NavDrawerActivity.java
+++ app/src/main/java/com/sheyi/testify/activity/NavDrawerActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -20,7 +21,12 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
 import com.makeramen.roundedimageview.RoundedTransformationBuilder;
+import com.pusher.client.Pusher;
+import com.pusher.client.channel.Channel;
+import com.pusher.client.channel.ChannelEventListener;
+import com.pusher.client.channel.SubscriptionEventListener;
 import com.sheyi.testify.R;
 import com.sheyi.testify.adapter.PostsAdapter;
 import com.sheyi.testify.helper.AuthenticationHelper;
@@ -28,6 +34,7 @@ import com.sheyi.testify.models.Post;
 import com.sheyi.testify.models.User;
 import com.sheyi.testify.rest.ApiClient;
 import com.sheyi.testify.rest.ApiInterface;
+import com.sheyi.testify.util.PusherUtil;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Transformation;
 
@@ -45,20 +52,32 @@ public class NavDrawerActivity extends AppCompatActivity
     private FragmentDrawer drawerFragment;
     private DrawerLayout drawer;
 
+    private SwipeRefreshLayout swipeRefreshContainer;
+
     private RecyclerView recycler;
     private PostsAdapter adapter;
 
     private ProgressBar loaderBar;
     private LinearLayout retryLL;
 
+    private List<Post> posts = new ArrayList<>();
+    private Pusher pusher;
+    private Channel postsStream;
+
+    private static String POST_CHANNEL = "posts-stream";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nav_drawer);
-        mToolbar = (Toolbar) findViewById(R.id.toolbar);
 
-        loaderBar = (ProgressBar) findViewById(R.id.homeSpinner);
-        retryLL = (LinearLayout) findViewById(R.id.retryLL);
+        pusher = PusherUtil.getInstance(this);
+
+        getByIDs();
+
+        adapter = new PostsAdapter(NavDrawerActivity.this, posts);
+        recycler.setLayoutManager(new LinearLayoutManager(NavDrawerActivity.this));
+        recycler.setAdapter(adapter);
 
         retryLL.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -67,9 +86,17 @@ public class NavDrawerActivity extends AppCompatActivity
                 Toast.makeText(NavDrawerActivity.this, "Reconnecting...", Toast.LENGTH_SHORT).show();
             }
         });
+
+        swipeRefreshContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                loadPosts();
+            }
+        });
+        swipeRefreshContainer.setColorSchemeResources(R.color.colorPrimary);
+
         setSupportActionBar(mToolbar);
 
-        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
@@ -81,6 +108,26 @@ public class NavDrawerActivity extends AppCompatActivity
         runSetup();
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        pusher.unsubscribe(POST_CHANNEL);
+    }
+
+    private void getByIDs() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        loaderBar = (ProgressBar) findViewById(R.id.homeSpinner);
+        retryLL = (LinearLayout) findViewById(R.id.retryLL);
+
+        swipeRefreshContainer = (SwipeRefreshLayout) findViewById(R.id.swipeRefreshContainer);
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        recycler = (RecyclerView) findViewById(R.id.postRecyclerView);
+
+    }
+
     private void runSetup() {
         if (AuthenticationHelper.isLoggedIn(this)) {
             initCurrentUser();
@@ -90,6 +137,7 @@ public class NavDrawerActivity extends AppCompatActivity
         getSupportActionBar().setTitle("Testify");
 
         loadPosts();
+        startPusher();
     }
 
     @Override
@@ -118,6 +166,10 @@ public class NavDrawerActivity extends AppCompatActivity
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        } else if (id == R.id.action_logout) {
+            AuthenticationHelper.logout(this);
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
         }
 
         return super.onOptionsItemSelected(item);
@@ -166,10 +218,13 @@ public class NavDrawerActivity extends AppCompatActivity
                 TextView currentUserNameTV = (TextView) findViewById(R.id.currentUserNameTV);
                 currentUserNameTV.setText(response.body().getName());
 
+                TextView currentUserEmailTV = (TextView) findViewById(R.id.currentUserEmailTV);
+                currentUserEmailTV.setText(response.body().getEmail());
+
                 ImageView currentUserAvatar = (ImageView) findViewById(R.id.currentUserAvatarIV);
                 Picasso.with(NavDrawerActivity.this)
-//                        .load(response.body().getAvatar())
-                        .load("http://lorempixel.com/400/200")
+                        .load(response.body().getAvatar())
+                        .error(R.drawable.testify_user_icon)
                         .transform(roundedTransformation)
                         .resize(100, 100)
                         .centerCrop()
@@ -194,10 +249,7 @@ public class NavDrawerActivity extends AppCompatActivity
     public void loadPosts() {
         loaderBar.setVisibility(View.VISIBLE);
         retryLL.setVisibility(View.GONE);
-
-        recycler = (RecyclerView) findViewById(R.id.postRecyclerView);
-
-        ArrayList<Post> posts = new ArrayList<>();
+        swipeRefreshContainer.setRefreshing(true);
 
         ApiInterface apiService = ApiClient.getApi(this);
 
@@ -205,12 +257,13 @@ public class NavDrawerActivity extends AppCompatActivity
         call.enqueue(new Callback<List<Post>>() {
             @Override
             public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
-                List<Post> posts = response.body();
-                adapter = new PostsAdapter(NavDrawerActivity.this, posts);
+                posts.clear();
+                posts.addAll(response.body());
+                adapter.notifyDataSetChanged();
 
-                recycler.setAdapter(adapter);
                 loaderBar.setVisibility(View.GONE);
                 retryLL.setVisibility(View.GONE);
+                swipeRefreshContainer.setRefreshing(false);
             }
 
             @Override
@@ -218,13 +271,53 @@ public class NavDrawerActivity extends AppCompatActivity
                 loaderBar.setVisibility(View.GONE);
                 retryLL.setVisibility(View.VISIBLE);
 
+                swipeRefreshContainer.setRefreshing(false);
+
                 Toast.makeText(NavDrawerActivity.this, "Couldn't not connect to internet", Toast.LENGTH_SHORT).show();
             }
         });
+    }
 
-        adapter = new PostsAdapter(NavDrawerActivity.this, posts);
-        recycler.setLayoutManager(new LinearLayoutManager(NavDrawerActivity.this));
-        recycler.setAdapter(adapter);
+    private void startPusher() {
+
+        postsStream = pusher.subscribe(POST_CHANNEL, new ChannelEventListener() {
+            @Override
+            public void onSubscriptionSucceeded(String s) {
+                NavDrawerActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(NavDrawerActivity.this, "Sub success", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onEvent(String s, String s1, String s2) {
+
+            }
+        });
+
+        postsStream.bind("new_post", new SubscriptionEventListener() {
+            @Override
+            public void onEvent(String channel, String event, final String data) {
+
+                NavDrawerActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Gson g = new Gson();
+                        Post p = g.fromJson(data, Post.class);
+
+                        posts.add(0, p);
+                        adapter.notifyItemInserted(0);
+
+                        recycler.scrollToPosition(0);
+                    }
+                });
+            }
+        });
+
+
+        pusher.connect();
     }
 
 }
