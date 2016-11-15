f20f7a7655fb06d57a38e966176d0011a3153b84
Seyi Adekoya <sheyooo@immortal.local>
2016-11-16 00:02:56 +0100

    checking user

--- app/src/main/java/com/sheyi/testify/activity/UserProfileActivity.java
+++ app/src/main/java/com/sheyi/testify/activity/UserProfileActivity.java
@@ -33,7 +33,9 @@ public class UserProfileActivity extends AppCompatActivity {
         call.enqueue(new Callback<User>() {
             @Override
             public void onResponse(Call<User> call, Response<User> response) {
-
+                if (response.isSuccessful()) {
+                    
+                }
             }
 
             @Override
